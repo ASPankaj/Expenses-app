3c26f954f85ef51c0b528073059025008c8907f4
ASPankaj <65583323+ASPankaj@users.noreply.github.com>
2020-07-04 09:44:52 +0530

    Updated version 1.3

---
+++ target/bits.pilani-0.0.1-SNAPSHOT/WEB-INF/classes/static/main.js
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+
+
+
+$('document').ready(function(){	
+	$('.table #editButton').on('click',function(event){	
+		
+		event.preventDefault();  		
+		var href = $(this).attr('href');
+		
+		$.get(href, function(expense, status){
+			 $('#idEdit').val(expense.id);
+			 $('#nameEdit').val(expense.name);
+			 $('#priceEdit').val(expense.price);
+			 $('#dateEdit').val(expense.date); 			
+		});	
+		
+		
+		
+		console.log("im here coming in this");
+		$('#editModal').modal();				
+	});	 
+	
+	$('.table #deleteButton').on('click',function(event){		
+		
+		event.preventDefault(); 
+		var href = $(this).attr('href');
+		$('#deleteModal #delRef').attr('href', href);
+		$('#deleteModal').modal();
+		
+	});
+	
+	
+	$('#dropdownMenuLink').on('click',function(event){		
+		
+		event.preventDefault(); 
+		
+		$('#dropdownLink').modal();
+		
+	});
+	
+	
+});
